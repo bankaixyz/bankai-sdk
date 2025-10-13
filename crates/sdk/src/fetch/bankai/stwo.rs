0b5a9c40d94ef9d845ed353cc4b2cca0a5522fab
petscheit <etscheitp@gmail.com>
2025-10-13 11:34:09 +0200

    feat: support serde serialized proofs

--- crates/sdk/src/fetch/bankai/stwo.rs
+++ crates/sdk/src/fetch/bankai/stwo.rs
@@ -1,7 +1,9 @@
 use cairo_air::CairoProof;
+use serde_json::Value;
+use starknet_ff::FieldElement;
 use stwo::core::vcs::blake2_merkle::Blake2sMerkleHasher;
-
-use crate::errors::{SdkError, SdkResult};
+use stwo_cairo_serialize::deserialize::CairoDeserialize;
+use crate::errors::SdkResult;
 use crate::fetch::clients::bankai_api::ApiClient;
 
 pub async fn fetch_block_proof(
@@ -9,7 +11,15 @@ pub async fn fetch_block_proof(
     block_number: u64,
 ) -> SdkResult<CairoProof<Blake2sMerkleHasher>> {
     let proof = client.get_block_proof(block_number).await?;
-    let proof: CairoProof<Blake2sMerkleHasher> =
-        serde_json::from_value(proof.proof).map_err(SdkError::from)?;
-    Ok(proof)
+    let value: Value = proof.proof;
+
+    // Try JSON struct first
+    if let Ok(parsed) = serde_json::from_value::<CairoProof<Blake2sMerkleHasher>>(value.clone()) {
+        return Ok(parsed);
+    } else {
+        let data: Vec<FieldElement> = value.as_array().unwrap().iter().map(|v| v.as_str().unwrap().parse().unwrap()).collect();
+        let res = CairoProof::<Blake2sMerkleHasher>::deserialize(&mut data.iter());
+        return Ok(res);
+
+    }
 }
